684d635760938b09b038fd8187369ecb67dd6f74
jiangshihao <jiangshihao@bytedance.com>
2020-11-02 11:39:05 +0800

    first commit
    Change-Id: If8e054ffd303d69bbb618d295405693606746a97

---
+++ .vuepress/config.js
@@ -0,0 +1,89 @@
+module.exports = {
+  "title": "Nick的学习笔记",
+  "description": "记录学习点滴",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    subSidebar: 'auto',
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      /*{
+        "text": "Docs",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },*/
+     /* {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }*/
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "nick",
+    "authorAvatar": "/avatar.png",
+    "record": "",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ blogs/frontend/test.md
@@ -0,0 +1,18 @@
+---
+title: 这是个测试问卷
+date: 2020-11-01
+tags:
+ - JavaScript
+categories:
+ -  frontend
+---
+
+
+## a
+
+hello
+
+
+## b
+
+world
